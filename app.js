a158add77317a03d4e845033b3f0a71c79780cd3
ElliotRespository <115067290+ElliotRespository@users.noreply.github.com>
2022-10-05 10:57:01 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+//Ny kommentar
+console.log("Hello World");
+
+console.log("On-site edit");
+
+function masterBranch() {
+    return 'masterBranch';
+}
+
+function sideBranch() {
+    return 'SideBranch';
+}
+
+function buttonPressed() {
+    return 'Du har klickat på knappen';
+}
